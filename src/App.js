07364326982b035fb3db3c766c7b05c66b6ede90
brettl1991 <brettl1991@gmail.com>
2023-01-27 17:09:51 +0000

    feat: setting up authentication and creating auth component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import "./App.css";
+import { Auth } from "./components/Auth";
+import Cookies from "universal-cookie";
+
+const cookies = new Cookies();
 
 function App() {
-  return <div>Hello World</div>;
+  const [isAuth, setIsAuth] = useState(cookies.get("auth-token"));
+  const [room, setRoom] = useState(null);
+
+  const roomInputRef = useRef(null);
+
+  if (!isAuth) {
+    return (
+      <div>
+        <Auth setIsAuth={setIsAuth} />
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      {room ? (
+        <div>Chat</div>
+      ) : (
+        <div className="room">
+          <label>Enter Room Name</label>
+          <input ref={roomInputRef} />
+          <button onClick={() => setRoom(roomInputRef.current.value)}>
+            Enter Chat
+          </button>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/firebase-config.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDQ4lJnepCYVVFXVHncFBklZRxMZvji7wg",
+  authDomain: "real-time-chatapp-621bd.firebaseapp.com",
+  projectId: "real-time-chatapp-621bd",
+  storageBucket: "real-time-chatapp-621bd.appspot.com",
+  messagingSenderId: "1019095966906",
+  appId: "1:1019095966906:web:452cbb3612f67421b94e83",
+  measurementId: "G-S93C0XV64B",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
