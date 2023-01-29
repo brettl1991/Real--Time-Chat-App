9323438aec119005b92d1f8e1d2dc91f46eaa4c0
brettl1991 <brettl1991@gmail.com>
2023-01-29 16:09:46 +0000

    add missing dependencies to useEffect

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -35,7 +35,7 @@ export const Chat = (props) => {
     });
 
     return () => unsubscribe();
-  }, []);
+  }, [messagesRef, room]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
