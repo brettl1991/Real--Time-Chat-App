be255a01521f392ce7ca0e93537aa46ddbdd7327
brettl1991 <brettl1991@gmail.com>
2023-01-29 15:56:27 +0000

    remove api keys from public

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -3,20 +3,12 @@ import { initializeApp } from "firebase/app";
 
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import firebaseConfig from "../helpers/apiKeys";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDQ4lJnepCYVVFXVHncFBklZRxMZvji7wg",
-  authDomain: "real-time-chatapp-621bd.firebaseapp.com",
-  projectId: "real-time-chatapp-621bd",
-  storageBucket: "real-time-chatapp-621bd.appspot.com",
-  messagingSenderId: "1019095966906",
-  appId: "1:1019095966906:web:452cbb3612f67421b94e83",
-  measurementId: "G-S93C0XV64B",
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
